6ec52d46cdeb5f39d7f6de8afd60f2f567ca398a
Sameer Brenn <sbrenn@twitter.com>
2019-04-27 15:45:42 -0600

    init

---
+++ src/main/scala/com/github/sameerparekh/holidays/package.scala
@@ -0,0 +1,156 @@
+package com.github.sameerparekh
+
+import com.github.nscala_time.time.Imports._
+import org.joda.time.DateTimeConstants._
+
+import scala.util.{Failure, Success, Try}
+
+package object holidays {
+  type HolidayFromYear = Int => LocalDate
+  type HolidaysForYear = Int => Set[LocalDate]
+
+  implicit class RichLocalDate(date: LocalDate) {
+    def isHoliday(holidaysForYear: HolidaysForYear): Boolean = {
+      holidaysForYear(date.getYear).contains(date)
+    }
+  }
+
+  val NewYears: HolidayFromYear = year => {
+    moveToWeekday(new LocalDate(year, JANUARY, 1))
+  }
+
+  val MLKDay: HolidayFromYear = year => {
+    getDayInRange(nthWeekdayOfMonthRange(3), MONDAY, JANUARY, year).get
+  }
+
+  val PresidentsDay: HolidayFromYear = year => {
+    getDayInRange(nthWeekdayOfMonthRange(3), MONDAY, FEBRUARY, year).get
+  }
+
+  val FamilyDay: HolidayFromYear = year => {
+    getDayInRange(nthWeekdayOfMonthRange(3), MONDAY, FEBRUARY, year).get
+  }
+
+  val VictoriaDay: HolidayFromYear = year => {
+    getDayInRange(18 to 24, MONDAY, MAY, year).get
+  }
+
+  val MemorialDay: HolidayFromYear = year => {
+    getDayInRange(lastWeekdayOfMonthRange(MAY, year), 1, 5, year).get
+  }
+
+  val CanadaDay: HolidayFromYear = year => {
+    moveToWeekday(new LocalDate(year, JULY, 1))
+  }
+
+  val IndependenceDay: HolidayFromYear = year => {
+    moveToWeekday(new LocalDate(year, JULY, 4))
+  }
+
+  val CivicHoliday: HolidayFromYear = year => {
+    getDayInRange(nthWeekdayOfMonthRange(1), MONDAY, AUGUST, year).get
+  }
+
+  val LaborDay: HolidayFromYear = year => {
+    getDayInRange(nthWeekdayOfMonthRange(1), MONDAY, SEPTEMBER, year).get
+  }
+
+  val ColumbusDay: HolidayFromYear = year => {
+    getDayInRange(nthWeekdayOfMonthRange(2), MONDAY, OCTOBER, year).get
+  }
+
+  val VeteransDay: HolidayFromYear = year => {
+    moveToWeekday(new LocalDate(year, NOVEMBER, 11))
+  }
+
+  val CanadianThanksgivingDay: HolidayFromYear = year => {
+    getDayInRange(nthWeekdayOfMonthRange(2), MONDAY, OCTOBER, year).get
+  }
+
+  val RemembranceDay: HolidayFromYear = year => {
+    moveToWeekday(new LocalDate(year, NOVEMBER, 11))
+  }
+
+  val ThanksgivingDay: HolidayFromYear = year => {
+    getDayInRange(nthWeekdayOfMonthRange(4), THURSDAY, NOVEMBER, year).get
+  }
+
+  val AfterThanksgivingDay: HolidayFromYear = year => {
+    ThanksgivingDay(year) + 1.day
+  }
+
+  val ChristmasEve: HolidayFromYear = year => {
+    moveToWeekday(new LocalDate(year, DECEMBER, 24))
+  }
+
+  val ChristmasDay: HolidayFromYear = year => {
+    moveToWeekday(new LocalDate(year, DECEMBER, 25))
+  }
+
+  val BoxingDay: HolidayFromYear = year => {
+    moveToWeekday(new LocalDate(year, DECEMBER, 26))
+  }
+
+  val NewYearsEve: HolidayFromYear = year => {
+    moveToWeekday(new LocalDate(year, DECEMBER, 31))
+  }
+
+  val USAFederalHolidays: HolidaysForYear = year => {
+    Set(
+      NewYears,
+      MLKDay,
+      PresidentsDay,
+      MemorialDay,
+      IndependenceDay,
+      LaborDay,
+      ColumbusDay,
+      VeteransDay,
+      ThanksgivingDay,
+      ChristmasDay,
+    ).map(_(year))
+  }
+
+  private def getDayInRange(
+    range: Seq[Int],
+    weekDay: Int,
+    month: Int,
+    year: Int
+  ): Try[LocalDate] = {
+    val matches = range.map { day =>
+      new LocalDate(year, month, day)
+    }.collect {
+      case dt if dt.getDayOfWeek == weekDay => dt
+    }
+
+    if (matches.size != 1) {
+      Failure(new RuntimeException(s"Unable to find a $weekDay of $month in $range"))
+    } else {
+      Success(matches.head)
+    }
+  }
+
+  private def nthWeekdayOfMonthRange(n: Int): Seq[Int] = {
+    ((n - 1) * 7) + 1 to n * 7
+  }
+
+  private def lastWeekdayOfMonthRange(month: Int, year: Int): Seq[Int] = {
+    val lastDayOfMonth = new LocalDate(year, month, 1)
+      .dayOfMonth
+      .withMaximumValue
+      .getDayOfMonth
+
+    (lastDayOfMonth - 6) to lastDayOfMonth
+  }
+
+  private def moveToWeekday(dt: LocalDate): LocalDate = {
+    // Move Saturdays to Friday and Sundays to Monday
+    dt.getDayOfWeek match {
+      case SUNDAY =>
+        dt + 1.day
+      case SATURDAY =>
+        dt - 1.day
+      case _ =>
+        dt
+    }
+  }
+}
---
+++ src/test/scala/com/github/sameerparekh/holidays/UsaSuite.scala
@@ -0,0 +1,48 @@
+package com.github.sameerparekh.holidays
+
+import com.github.nscala_time.time.Imports.{LocalDate, _}
+import org.joda.time.DateTimeConstants.JANUARY
+import org.scalatest.{FunSuite, MustMatchers}
+
+class UsaSuite extends FunSuite with MustMatchers {
+  def daysInYear(year: Int): Seq[LocalDate] = {
+    def listDays(startDay: LocalDate): Stream[LocalDate] = {
+      startDay #:: listDays(startDay + 1.day)
+    }
+    listDays(new LocalDate(year, JANUARY, 1)).takeWhile(_.getYear == year)
+  }
+
+  test("2018 USA holidays should be correct") {
+    val holidays = daysInYear(2018).filter(_.isHoliday(USAFederalHolidays)).map(_.toString)
+
+    holidays must contain theSameElementsAs Seq(
+      "2018-01-01",
+      "2018-01-15",
+      "2018-02-19",
+      "2018-05-28",
+      "2018-07-04",
+      "2018-09-03",
+      "2018-10-08",
+      "2018-11-22",
+      "2018-12-25",
+    )
+  }
+
+  test("USA 2019 Holidays should be correct") {
+    val holidays = daysInYear(2019).filter(_.isHoliday(USAFederalHolidays)).map(_.toString)
+
+    holidays must contain theSameElementsAs Seq(
+      "2019-01-01",
+      "2019-01-21",
+      "2019-02-18",
+      "2019-05-27",
+      "2019-07-04",
+      "2019-09-02",
+      "2019-10-14",
+      "2019-11-11",
+      "2019-11-28",
+      "2019-12-25",
+    )
+  }
+
+}
